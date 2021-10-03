e8be49c48a0430cd80ce239aaed8d4c9883e89fb
SMAkbar <sm.akbar42@gmail.com>
2021-10-03 20:56:21 +0500

    New File added

---
+++ src/ai/vyro/NewFile.java
@@ -0,0 +1,11 @@
+package ai.vyro;
+
+public class NewFile {
+    public NewFile(){
+        System.out.println("Adding a new file here");
+    }
+
+    public void print(){
+        System.out.println("Successfully Added a New File");
+    }
+}
